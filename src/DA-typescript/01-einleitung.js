1c62107b42f804a7d8d2e4059fe1a6f12a21bc37
hanbit10 <chanbit10@gmail.com>
2024-06-23 20:58:48 +0200

    update typescript basic

---
+++ src/DA-typescript/01-einleitung.js
@@ -0,0 +1,6 @@
+var theString = "das ist ein string";
+var theNumber = 123123;
+var theBoolean = true;
+var theUndefined = undefined;
+var theNull = null;
+var theAnyType = "hello";
---
+++ src/DA-typescript/01-einleitung.ts
@@ -0,0 +1,7 @@
+let theString: string = "das ist ein string";
+let theNumber: number = 123123;
+let theBoolean: boolean = true;
+
+let theUndefined = undefined;
+let theNull = null;
+let theAnyType: any = "hello";
