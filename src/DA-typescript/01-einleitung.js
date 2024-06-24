98e939e233273ebc9c763eba2eae67fcf8616102
hanbit10 <chanbit10@gmail.com>
2024-06-24 13:26:35 +0200

    update tutorials

--- src/DA-typescript/01-einleitung.js
+++ src/DA-typescript/01-einleitung.js
@@ -4,3 +4,35 @@ var theBoolean = true;
 var theUndefined = undefined;
 var theNull = null;
 var theAnyType = "hello";
+var Otherperson = /** @class */ (function () {
+    function Otherperson() {
+    }
+    return Otherperson;
+}());
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.theArray = [1, 2];
+        this.noteType = "hanbit";
+        this.theTuple = [200, 200];
+        this.person = {
+            name: "Florian",
+            age: 50,
+        };
+        this.intPerson = {
+            name: "Florian",
+            age: 50,
+        };
+        this.otherPerson = {
+            name: "Florian",
+            age: 50,
+        };
+        this.theTypeless = "asdasd";
+        this.theArray = [1, 2, 3, 4, "d"];
+        this.add(1, 2);
+        this.noteType = "hallo";
+    }
+    AppComponent.prototype.add = function (a, b) {
+        return a + b;
+    };
+    return AppComponent;
+}());
--- src/DA-typescript/01-einleitung.ts
+++ src/DA-typescript/01-einleitung.ts
@@ -5,3 +5,55 @@ let theBoolean: boolean = true;
 let theUndefined = undefined;
 let theNull = null;
 let theAnyType: any = "hello";
+
+interface Person {
+  name: string | number;
+  age: number;
+  position?: string;
+}
+
+class Otherperson {
+  name: string | number | undefined;
+  age: number | undefined;
+  position?: string;
+}
+
+class AppComponent {
+  theTypeless: undefined | string;
+  theArray: (number | string)[] = [1, 2];
+  noteType: "hanbit" | "hallo" = "hanbit";
+  theTuple: [number, number] = [200, 200];
+  person: { readonly name: string | number; age: number; position?: string } = {
+    name: "Florian",
+    age: 50,
+  };
+
+  intPerson: Person = {
+    name: "Florian",
+    age: 50,
+  };
+
+  otherPerson: Otherperson = {
+    name: "Florian",
+    age: 50,
+  };
+
+  fruit? = "";
+  fruitList = ["Banana"];
+
+  constructor() {
+    this.theTypeless = "asdasd";
+    this.theArray = [1, 2, 3, 4, "d"];
+    this.add(1, 2);
+    this.noteType = "hallo";
+    this.fruit = this.fruitList.pop();
+    let banana = this.fruitList.pop();
+    if (banana != undefined) {
+      this.fruit = banana;
+    }
+  }
+
+  add(a: number, b: number) {
+    return a + b;
+  }
+}
